d60a166f87ac888436cb7dfbad3ecc3cc90f8c09
Shaon Borosha <shaon.borosha@vanderbilt.edu>
2020-09-30 17:43:14 -0500

    all done. not pretty

--- Fall2020_Web/Web_ICP5/Source/ToDoICP/src/app/app.component.ts
+++ Fall2020_Web/Web_ICP5/Source/ToDoICP/src/app/app.component.ts
@@ -3,26 +3,41 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
 
+export class AppComponent {
   // define list of items
   items= [];
 
   // Write code to push new item
-  submitNewItem() {
-
+  submitNewItem(itemStr: string): void {
+    if(this.items.indexOf(itemStr) != -1){
+      alert("This item already exists! Write a different item description.");
+    } else{
+      this.items.push(itemStr);
+    }
+    console.log(this.items);
   }
 
   // Write code to complete item
-  completeItem() {
-
+  completeItem(itemStr: string): void {
+    let index: number = this.items.indexOf(itemStr);
+    if(index != -1){
+      let tbl = (<HTMLTableElement >document.getElementById("todo-list"));
+      tbl.rows[index].style.backgroundColor = "orange";
+      let item = tbl.rows[index].cells[1];
+      let itemStr = item.innerHTML;
+      item.innerHTML = itemStr.strike();
+    }
+    console.log(this.items);
   }
 
   // Write code to delete item
-  deleteItem() {
-
+  deleteItem(itemStr:string): void {
+    let index: number = this.items.indexOf(itemStr);
+    if(index != -1){
+      this.items.splice(index,1);
+    }
+    console.log(this.items);
   }
-
 }
