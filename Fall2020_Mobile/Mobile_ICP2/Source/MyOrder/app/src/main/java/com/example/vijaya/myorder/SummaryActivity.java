464a649feae4e7b6081cb8324b1b2258d495454b
Shaon Borosha <shaon.borosha@vanderbilt.edu>
2020-10-29 19:54:29 -0500

    added the needed files

---
+++ Fall2020_Mobile/Mobile_ICP2/Source/MyOrder/app/src/main/java/com/example/vijaya/myorder/SummaryActivity.java
@@ -0,0 +1,11 @@
+package com.example.vijaya.myorder;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+public class SummaryActivity extends AppCompatActivity {
+    // create a text view in the activity that holds the summary from main java
+
+    // redirect to order page when go to order button is clicked
+    public void goToOrder(){
+    }
+}
